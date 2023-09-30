5fae157c053fe13ade09840f8f53fb9ff446c4e6
Roderick <Roderickj800@gmail.com>
2023-09-30 16:44:13 -0400

    finsiehd with the spotImage model

---
+++ backend/db/seeders/20230930203245-initialImage.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+const { SpotImage } = require("../models");
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await SpotImage.bulkCreate(
+      {
+        spotId: 1,
+        url: "https://i.pinimg.com/originals/2c/57/27/2c5727c9b2409b567f7f10207ae8d0b8.jpg",
+        preview: true,
+      },
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = "SpotImages";
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(
+      options,
+      {
+        spotId: {
+          [Op.in]: [1],
+        },
+      },
+      {}
+    );
+  },
+};
