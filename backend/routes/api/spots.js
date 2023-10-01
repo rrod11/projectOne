7400505f10824e399faab1db190ceb832f14fd7e
Roderick <Roderickj800@gmail.com>
2023-10-01 13:17:36 -0400

    see what i get

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,13 +2,19 @@ const express = require("express");
 // const bcrypt = require("bcryptjs");
 // const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { Spot } = require("../../db/models");
+const { SpotImage } = require("../../db/models");
 // const { check } = require("express-validator");
 // const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const spots = await Spot.findAll();
-  console.log(spots);
+  const spots = await Spot.findAll({
+    include: {
+      model: SpotImage,
+      attributes: ["url"],
+    },
+  });
+  // console.log(spots);
   return res.json(spots);
 });
 
