2d4d2eacf1f4fde5536cc4c5297ed40a64ab5342
Roderick <Roderickj800@gmail.com>
2023-10-01 12:41:00 -0400

    maybe the merge conflict was the issue

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -5,12 +5,9 @@ const { Spot } = require("../../db/models");
 // const { check } = require("express-validator");
 // const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
-<<<<<<< HEAD
-=======
 
 router.get("/", async (req, res) => {
   const spots = await Spot.findAll();
 
   res.json(spots);
 });
->>>>>>> spot
