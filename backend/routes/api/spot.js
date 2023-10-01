187ea2694a5b13b866b4578ab0a75193adb78287
Roderick <Roderickj800@gmail.com>
2023-10-01 12:18:46 -0400

    trying my first solo router

---
+++ backend/routes/api/spot.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// const bcrypt = require("bcryptjs");
+// const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { Spot } = require("../../db/models");
+// const { check } = require("express-validator");
+// const { handleValidationErrors } = require("../../utils/validation");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const spots = await Spot.findAll();
+
+  res.json(spots);
+});
