cff9f337d3691e952aa93e72c6d5656113779174
Roderick <Roderickj800@gmail.com>
2023-10-03 13:29:44 -0400

    think i got the details to work

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { Spot } = require("../../db/models");
 const { SpotImage } = require("../../db/models");
 const { Review } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth.js");
 
 // const { check } = require("express-validator");
 // const { handleValidationErrors } = require("../../utils/validation");
@@ -41,7 +42,7 @@ router.get("/", async (req, res) => {
   }
   res.json({ Spots: spotsJSON });
 });
-router.get("/current", async (req, res) => {
+router.get("/current", requireAuth, async (req, res) => {
   const { user } = req;
   const userId = user.id;
   const spots = await Spot.unscoped().findAll({
@@ -61,6 +62,44 @@ router.get("/current", async (req, res) => {
     if (spotsJSON[i].SpotImages.length > 0) {
       spotsJSON[i].previewImage = spotsJSON[i].SpotImages[0].url;
       delete spotsJSON[i].SpotImages;
+    }
+  }
+  for (let spot of spotsJSON) {
+    const sum = await Review.sum("stars", {
+      where: {
+        spotId: spot.id,
+      },
+    });
+    const total = await Review.count({
+      where: {
+        spotId: spot.id,
+      },
+    });
+    spot.avgRating = sum / total;
+  }
+  res.json({ Spots: spotsJSON });
+});
+router.get("/:spotId", async (req, res) => {
+  const spotId = req.params.spotId;
+  // console.log("MY CURRENT ID:", spotId);
+  const spots = await Spot.unscoped().findAll({
+    where: {
+      id: spotId,
+    },
+    include: [
+      {
+        model: SpotImage,
+      },
+    ],
+  });
+  const spotsJSON = spots.map((ele) => ele.toJSON());
+
+  for (let i = 0; i < spotsJSON.length; i++) {
+    console.log("MY SPOT IMAGES!!:", spotsJSON[i].SpotImages);
+    if (spotsJSON[i].SpotImages.length > 1) {
+      for (let j = 1; j < spotsJSON[i].SpotImages.length; j++) {
+        spotsJSON[i].SpotImages[j].preview = false;
+      }
     }
   }
   for (let spot of spotsJSON) {
