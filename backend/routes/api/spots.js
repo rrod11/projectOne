6844a6d6a4ccf507cfff4e63e0a27522c2a19669
Roderick <Roderickj800@gmail.com>
2023-10-03 12:27:10 -0400

    got all spots to return

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,20 +3,43 @@ const express = require("express");
 // const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { Spot } = require("../../db/models");
 const { SpotImage } = require("../../db/models");
+const { Review } = require("../../db/models");
+
 // const { check } = require("express-validator");
 // const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const spots = await Spot.findAll({
-    include: {
-      model: SpotImage,
-      attributes: ["url"],
-      as: "previewImage",
-    },
+  const spots = await Spot.unscoped().findAll({
+    include: [
+      {
+        model: SpotImage,
+        attributes: ["url"],
+      },
+    ],
   });
-  // console.log(spots);
-  return res.json(spots);
+  const spotsJSON = spots.map((ele) => ele.toJSON());
+
+  for (let i = 0; i < spotsJSON.length; i++) {
+    if (spotsJSON[i].SpotImages.length > 0) {
+      spotsJSON[i].previewImage = spotsJSON[i].SpotImages[0].url;
+      delete spotsJSON[i].SpotImages;
+    }
+  }
+  for (let spot of spotsJSON) {
+    const sum = await Review.sum("stars", {
+      where: {
+        spotId: spot.id,
+      },
+    });
+    const total = await Review.count({
+      where: {
+        spotId: spot.id,
+      },
+    });
+    spot.avgRating = sum / total;
+  }
+  res.json({ Spots: spotsJSON });
 });
 
 module.exports = router;
