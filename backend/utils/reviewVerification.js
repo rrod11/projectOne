546ce838ac03c7fd37361e0211418b4eabe839a4
Roderick <Roderickj800@gmail.com>
2023-10-04 12:09:36 -0400

    got the review image working

--- backend/utils/reviewVerification.js
+++ backend/utils/reviewVerification.js
@@ -1,3 +1,4 @@
+const { Review, User, ReviewImage } = require("../db/models");
 const reviewVerification = (req, res, next) => {
   const { review, stars } = req.body;
   const err = {};
@@ -19,5 +20,23 @@ const reviewVerification = (req, res, next) => {
     next();
   }
 };
+const reviewLengthVerification = async (req, res, next) => {
+  const reviewId = req.params.reviewId;
+  const { user } = req;
+  const err = {};
+  err.message = "Maximum number of images for this resource was reached";
+  const target = await ReviewImage.count({
+    where: {
+      reviewId: reviewId,
+    },
+  });
+  console.log("HERES THE REVIEWS HERE:", target);
+  if (target >= 10) {
+    err.title = "Can't Have More than 10 Reviews";
+    err.status = 403;
+    next(err);
+  } else next();
+};
 
 module.exports = reviewVerification;
+module.exports = reviewLengthVerification;
