919db4183008ce92f6aa09d0262b21b79b8e9cc7
Roderick <Roderickj800@gmail.com>
2023-10-05 13:42:46 -0400

    login updated

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,6 +10,12 @@ const validateSignup = [
     .exists({ checkFalsy: true })
     .isEmail()
     .withMessage("Please provide a valid email."),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage("First Name is required."),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("Last Name is required."),
   check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
@@ -46,29 +52,5 @@ router.post("/", validateSignup, async (req, res) => {
     user: safeUser,
   });
 });
-router.post("/", async (req, res) => {
-  const { email, password, username, firstName, lastName } = req.body;
-  const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({
-    email,
-    username,
-    hashedPassword,
-    firstName,
-    lastName,
-  });
 
-  const safeUser = {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    username: user.username,
-  };
-
-  await setTokenCookie(res, safeUser);
-
-  return res.json({
-    user: safeUser,
-  });
-});
 module.exports = router;
