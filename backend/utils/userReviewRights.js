88e5edcb3bebd99b4103fcee5a92060d86a5f0a6
Roderick <Roderickj800@gmail.com>
2023-10-06 20:00:40 -0400

    finalizing agin

--- backend/utils/userReviewRights.js
+++ backend/utils/userReviewRights.js
@@ -4,21 +4,17 @@ const userReviewRightsAuthentication = async (req, _res, next) => {
   const { user } = req;
   const userId = user.id
   const err = {};
-  err.message = "Review couldn't be found";
+  err.message = "Forbidden";
   const target = await Review.findOne({
     where: {
       id: reviewId,
     },
   });
 
-  if ( target == null) {
-    err.title = "Couldn't find a Review with the specified id";
-    err.status = 404;
-    next(err);
-  }
+
   if (  userId != target.userId) {
-    err.title = "Couldn't find a Review with the specified id";
-    err.status = 404;
+    err.title = "Unauthorized Permission";
+    err.status = 403;
     next(err);
   }
    next();
