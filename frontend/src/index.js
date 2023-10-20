394ba507d0bcd9e154acc47f206a0331a5cd92ce
Roderick <Roderickj800@gmail.com>
2023-10-20 13:23:02 -0400

    part 4 almost done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,13 @@ import "./index.css";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { restoreCSRF, csrfFetch } from "./store/csrf";
-import * as sessionActions from "./store/session";
+import { ModalProvider, Modal } from "./context/Modal";
 import App from "./App";
 
 import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
@@ -19,15 +21,20 @@ if (process.env.NODE_ENV !== "production") {
   window.store = store;
   window.sessionActions = sessionActions;
 }
+
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
+
 ReactDOM.render(
   <React.StrictMode>
     <Root />
