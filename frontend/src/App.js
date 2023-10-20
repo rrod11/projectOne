4216b8ed94d221ee3d51030d1cea6838b357734b
Roderick <Roderickj800@gmail.com>
2023-10-20 12:48:49 -0400

    finished 3 authme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
