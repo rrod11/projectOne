f4e848d795eb0b34a6afa70ccfd400e8ec0b32e5
Roderick <Roderickj800@gmail.com>
2023-10-23 14:06:30 -1000

    finished

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { allTheSpots } from "../../store/spots";
 import { useEffect } from "react";
-
+import "./landingPage.css";
 const LandingPage = () => {
   const dispatch = useDispatch();
   const spots = useSelector((state) => state.spots.Spots);
@@ -10,13 +10,24 @@ const LandingPage = () => {
   }, [dispatch]);
 
   console.log("🚀 ~ file: index.js:4 ~ spot:", spots);
+  function myFunction(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    // document.getElementByClassName("tooltiptext").style.left = `${x} + "px"`;
+    // document.getElementByClassName("tooltiptext").style.top = `${y} + "px"`;
+  }
   return !spots ? null : (
-    <div className="LPImages">
+    <div className="LPImages" onMouseMove={myFunction}>
       {spots?.map((ele) => (
-        <div key={ele.id}>
-          <img src={ele.previewImage} className="actualImages" />
+        <div key={ele.id} className="imageDiv">
+          <div className="tooltip">
+            <img src={ele.previewImage} className="actualImages" />
+            <p className="tooltiptext">{ele.name}</p>
+          </div>
           <div className="houseInfo">
-            <p>{ele.name}</p>
+            <p>
+              {ele.city}, {ele.state}
+            </p>
             <di className="ratingInfo">
               <i class="fa-solid fa-moon"></i>
               <p>{parseFloat(`${ele.avgRating}`).toFixed(2)}</p>
@@ -25,7 +36,6 @@ const LandingPage = () => {
           <p>${ele.price} /night</p>
         </div>
       ))}
-      FOUND ME
     </div>
   );
 };
