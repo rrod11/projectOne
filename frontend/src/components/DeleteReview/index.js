a4549b63f95932f328d41eb01f162c7f711e7ce7
Roderick <Roderickj800@gmail.com>
2023-10-30 05:51:37 -1000

    finished

--- frontend/src/components/DeleteReview/index.js
+++ frontend/src/components/DeleteReview/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { deleteAReview } from "../../store/reviews";
+import "./index.css";
 
 function DeleteAReviewModal({ reviewId }) {
   const dispatch = useDispatch();
@@ -16,20 +17,36 @@ function DeleteAReviewModal({ reviewId }) {
   };
   return (
     <>
-      <h1>Confirm Delete</h1>
-      <p>Are you sure you want to remove this review?</p>
-      <button
-        style={{ backgroundColor: "red", color: "white" }}
-        onClick={beginDelete}
+      <div
+        style={{
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          fontFamily: "Times New Roman",
+          justifyContent: "center",
+          padding: "25px",
+          width: "250px",
+        }}
       >
-        Yes(Delete Review)
-      </button>
-      <button
-        style={{ backgroundColor: "grey", color: "white" }}
-        onClick={closeScreen}
-      >
-        No(Keep Review)
-      </button>
+        <h1>Confirm Delete</h1>
+        <p>Are you sure you want to remove this review?</p>
+        <div>
+          <button
+            className="delete-button"
+            style={{ backgroundColor: "red", color: "white", width: "50%" }}
+            onClick={beginDelete}
+          >
+            Yes(Delete Review)
+          </button>
+          <button
+            className="update-button"
+            style={{ backgroundColor: "grey", color: "white", width: "50%" }}
+            onClick={closeScreen}
+          >
+            No(Keep Review)
+          </button>
+        </div>
+      </div>
     </>
   );
 }
